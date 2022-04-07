47f4b9203e9b60d17aa790b1de150d2df14ffc19
malorah <enzzo.ayala_p@mail.udp.cl>
2022-04-07 15:50:48 -0400

    form

---
+++ back/form.php
@@ -0,0 +1,41 @@
+<?php   
+session_start();
+if(isset($_SESSION['user'])){
+    ?>
+    <html>
+       <a>Hola,<?php echo $_SESSION['user']['name']?></a>
+    </html>
+   <?php
+}else{
+?>
+ <html>
+    <a>Registrese para poder guardar sus datos</a>
+ </html>
+<?php
+}
+$honorarios=0;
+$presuntos=$honorarios*0.3;
+$honorarioMax=0;
+if($presuntos>$honorarioMax){
+$presuntos=$honorarioMax;
+}
+$sueldos=0;
+$factor=0;
+$rebaja=0;
+$base_global=($sueldos+($honorarios-$presuntos));
+$final=($base*$factor)-($honorarios*$factor)
+if($final<0){
+    ?>
+ <html>
+    <a>Hay devolucion</a>
+ </html>
+<?php
+}else{
+    ?>
+ <html>
+    <a>Tiene que pagar impuestos</a>
+ </html>
+<?php
+}
+
+?>
