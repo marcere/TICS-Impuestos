b0fa8384829612dc6cd1c6729b020881984e37bb
malorah <enzzo.ayala_p@mail.udp.cl>
2022-04-06 22:45:58 -0400

    seudo inicio

---
+++ formulas.php
@@ -0,0 +1,5 @@
+gastos presuntos = sumatoria honorarios *0.3
+si gastos presuntos maximo
+base global =(sumatoria sueldos + sumatoria honoriarios - gastos presuntos)
+final = (base global * factor)- rebaja-(sumatoria honorarios * factor honorario) //factor honorario = 0.1225
+si final negativo = devolucion
